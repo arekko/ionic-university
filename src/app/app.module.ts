341c4449b2b15a44fd70ca2dced0bba22840318a
arekko <amlk158@gmail.com>
2019-01-16 19:12:58 +0200

    Fetching data with HTTP Client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,21 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
-
+import { HttpClientModule } from "@angular/common/http";
 import { PhotoViewer } from "@ionic-native/photo-viewer";
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
 
 @NgModule({
   declarations: [MyApp, HomePage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage],
   providers: [
     StatusBar,
     SplashScreen,
     PhotoViewer,
+    HttpClientModule,
     { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
